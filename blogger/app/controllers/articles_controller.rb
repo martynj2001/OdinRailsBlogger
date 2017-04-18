a0d1f181ad8528ea81aa783007b15692c30b99c3
Martyn <martynj2010@icloud.com>
2017-04-18 19:41:15 +0100

    Finished Jumpstartlab tutorial part 11

--- blogger/app/controllers/articles_controller.rb
+++ blogger/app/controllers/articles_controller.rb
@@ -14,25 +14,33 @@ class ArticlesController < ApplicationController
 		@article = Article.new #Used for reflection so the Rails form helper knows what display in the view.
 	end
 
-	def create
+	def create #Called from the New view (Submite Button)
 		@article = Article.new(article_params)
 		@article.save
+
+		flash.notice = "New Article '#{@article.title}' Created"
+
 		redirect_to article_path(@article)
 	end
 
 	def destroy
 		@articles = Article.find(params[:id])
 		@articles.destroy
-		redirect_to article_path(@articles.id + 1)
+
+		flash.notice = "Article Deleted"
+
+		redirect_to articles_path
 	end
 
 	def edit
 		@article = Article.find(params[:id])
 	end
 
-	def update
+	def update #Called from the edit view (Update button)
 		@article = Article.find(params[:id])
 		@article.update(article_params)
+
+		flash.notice = "Article '#{@article.title}' Updated"
 
 		redirect_to article_path(@article)
 	end
