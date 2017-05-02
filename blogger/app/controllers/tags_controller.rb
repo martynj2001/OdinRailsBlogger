0a68daf6dbac9f3b6fee6fd67238c88ed286f4d6
Martyn <martynj2010@icloud.com>
2017-05-02 21:58:10 +0100

    final commit - completed tutorial upto I5. Added user authentication and styling.

--- blogger/app/controllers/tags_controller.rb
+++ blogger/app/controllers/tags_controller.rb
@@ -1,4 +1,5 @@
 class TagsController < ApplicationController
+	before_filter :require_login, only: [:destroy]
 
 	def index
 		@tag = Tag.all
@@ -6,6 +7,10 @@ class TagsController < ApplicationController
 
 	def show
 		@tag = Tag.find(params[:id])
+	end
+
+	def destroy
+
 	end
 
 end
